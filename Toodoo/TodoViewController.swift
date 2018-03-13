c7ecc30f8e32689b63a377eb494c5d45c0d20edc
Mac Pro <macpro@rb-mac-pro.local>
2018-03-13 12:54:35 -0700

    Got checkmarks working

---
+++ Toodoo/TodoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Toodoo
+//
+//  Created by Mac Pro on 3/12/18.
+//  Copyright © 2018 RBond. All rights reserved.
+//
+
+import UIKit
+
+class TodoViewController: UITableViewController {
+
+    let itemArray = ["learn Xcode", "make app", "Sell app" ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        //print(indexPath.row)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+}
+
